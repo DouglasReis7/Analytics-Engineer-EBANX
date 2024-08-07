cfb70e320dc958391e6c20427fcad14928142a48
DouglasReis7 <61767068+DouglasReis7@users.noreply.github.com>
2024-08-07 22:35:07 +0000

    finished project structure

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,5 @@
-curl -sSL install.astronomer.io | sudo bash -s
-curl -sSL install.astronomer.io | sudo bash -s -- v1.28.0
 pip install -r requirements.txt
-bash -i -c 'astro dev start -n --wait 5m'
+cd dbt
+dbt build --full-refresh
+dbt docs generate
+dbt docs serve
